c79572619edaeade0464adab0c8c6d5cad7fa061
Derrick Eriksson <DERIKS23@volvocars.com>
2021-12-06 13:08:13 +0100

    map => []int, fewer loops as well

--- day06/aoc.go
+++ day06/aoc.go
@@ -2,39 +2,39 @@ package main
 import ( "fmt"; "os" )
 
 func getSolution(startingFish []int, days int) int {
-	fishPregnancies := map[int]int{}
-	longDays := days/4
+	var fishes [9]int
+	longDays := days/8
 	zero,one,two,three,four,five,six,seven,eigth := 0,0,0,0,0,0,0,0,0
 
 	for _ , daysLeft := range startingFish {
-		fishPregnancies[daysLeft]++
+		fishes[daysLeft]++
 	}
 
 	for i := 0; i < longDays; i++ {
-		zero,one,two,three,four,five,six,seven,eigth = fishPregnancies[0], fishPregnancies[1], fishPregnancies[2], fishPregnancies[3], fishPregnancies[4], fishPregnancies[5], fishPregnancies[6], fishPregnancies[7], fishPregnancies[8]
-		fishPregnancies[0] = four
-		fishPregnancies[1] = five
-		fishPregnancies[2] = six
-		fishPregnancies[3] = zero + seven // parents + newborn
-		fishPregnancies[4] = one + eigth // parents + newborn
-		fishPregnancies[5] = zero + two // parents + newborn
-		fishPregnancies[6] = one + three // parents + newborn
-		fishPregnancies[7] = two
-		fishPregnancies[8] = three
+		zero,one,two,three,four,five,six,seven,eigth = fishes[0], fishes[1], fishes[2], fishes[3], fishes[4], fishes[5], fishes[6], fishes[7], fishes[8]
+		fishes[0] = eigth + one // parent + newborn
+		fishes[1] = zero + two // parent + newborn
+		fishes[2] = one + three // parent + newborn
+		fishes[3] = two + four // parent + newborn
+		fishes[4] = three + five // parent + newborn
+		fishes[5] = four + six // parent + newborn
+		fishes[6] = five + seven + zero // parent + parent + newborn
+		fishes[7] = six // parent + newborn
+		fishes[8] = seven + zero // parent + newborn
 	}
 
 	totalFishes := 0
-	for _, numberOfFishWithXDaysLeft := range fishPregnancies {
-		totalFishes += numberOfFishWithXDaysLeft
+	for _, fish := range fishes {
+		totalFishes += fish
 	}
 
 	return totalFishes
 }
 
 func main() {
-	// testInput := []int{3,4,3,1,2}
 	input := []int{3,4,3,1,2,1,5,1,1,1,1,4,1,2,1,1,2,1,1,1,3,4,4,4,1,3,2,1,3,4,1,1,3,4,2,5,5,3,3,3,5,1,4,1,2,3,1,1,1,4,1,4,1,5,3,3,1,4,1,5,1,2,2,1,1,5,5,2,5,1,1,1,1,3,1,4,1,1,1,4,1,1,1,5,2,3,5,3,4,1,1,1,1,1,2,2,1,1,1,1,1,1,5,5,1,3,3,1,2,1,3,1,5,1,1,4,1,1,2,4,1,5,1,1,3,3,3,4,2,4,1,1,5,1,1,1,1,4,4,1,1,1,3,1,1,2,1,3,1,1,1,1,5,3,3,2,2,1,4,3,3,2,1,3,3,1,2,5,1,3,5,2,2,1,1,1,1,5,1,2,1,1,3,5,4,2,3,1,1,1,4,1,3,2,1,5,4,5,1,4,5,1,3,3,5,1,2,1,1,3,3,1,5,3,1,1,1,3,2,5,5,1,1,4,2,1,2,1,1,5,5,1,4,1,1,3,1,5,2,5,3,1,5,2,2,1,1,5,1,5,1,2,1,3,1,1,1,2,3,2,1,4,1,1,1,1,5,4,1,4,5,1,4,3,4,1,1,1,1,2,5,4,1,1,3,1,2,1,1,2,1,1,1,2,1,1,1,1,1,4}
 	
+	fmt.Println("Go")
 	part := os.Getenv("part")
 
 	if part == "part2" { fmt.Println(getSolution(input, 256))
